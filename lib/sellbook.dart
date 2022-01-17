53fe3cd88dba083e4b0510c9acfa84d8e18c5304
engelindo <engel_jp@hotmail.com>
2022-01-17 15:58:31 -0800

    many changes to sellbook

--- lib/sellbook.dart
+++ lib/sellbook.dart
@@ -3,11 +3,12 @@ import 'dart:io';
 import 'package:image_picker/image_picker.dart';
 
 String condition;
-int price;
+double price = 0;
 String sellerName;
 String course;
 String profName;
 String bookName;
+dynamic imageFile;
 
 class SellBook extends StatefulWidget {
   @override
@@ -15,7 +16,6 @@ class SellBook extends StatefulWidget {
 }
 
 class _SellBookState extends State<SellBook> {
-  dynamic imageFile;
   bool picked = false;
 
   /// Get from gallery
@@ -29,6 +29,7 @@ class _SellBookState extends State<SellBook> {
       imageFile = File(pickedFile.path);
       picked = true;
       setState(() {});
+      Navigator.of(context).pop();
     }
   }
 
@@ -45,210 +46,250 @@ class _SellBookState extends State<SellBook> {
       picked = true;
       print('im here');
       setState(() {});
+      Navigator.of(context).pop();
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(30.0),
-        child: ListView(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(right: 40.0),
-              child: TextFormField(
-                onChanged: (value) {
-                  bookName = value;
-                },
-                cursorColor: Theme.of(context).cursorColor,
-                initialValue: 'Insert Name',
-                maxLength: 20,
-                decoration: InputDecoration(
-                  icon: Icon(Icons.favorite),
-                  labelText: 'Book Name',
-                  labelStyle: TextStyle(
-                    color: Color(0xFF6200EE),
-                  ),
-                  helperText: 'Helper text',
-                  suffixIcon: Icon(
-                    Icons.check_circle,
-                  ),
-                  enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Color(0xFF6200EE)),
-                  ),
+      body: ListView(
+        // padding: ,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(right: 40.0),
+            child: TextFormField(
+              onChanged: (value) {
+                bookName = value;
+              },
+              cursorColor: Theme.of(context).cursorColor,
+              initialValue: 'Insert Name',
+              maxLength: 20,
+              decoration: InputDecoration(
+                icon: Icon(Icons.favorite),
+                labelText: 'Book Name',
+                labelStyle: TextStyle(
+                  color: Color(0xFF6200EE),
+                ),
+                helperText: 'Helper text',
+                suffixIcon: Icon(
+                  Icons.check_circle,
+                ),
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Color(0xFF6200EE)),
                 ),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.only(right: 40.0),
-              child: TextFormField(
-                onChanged: (value) {
-                  profName = value;
-                },
-                cursorColor: Theme.of(context).cursorColor,
-                initialValue: 'Insert Name',
-                maxLength: 20,
-                decoration: InputDecoration(
-                  icon: Icon(Icons.favorite),
-                  labelText: 'Professor Name',
-                  labelStyle: TextStyle(
-                    color: Color(0xFF6200EE),
-                  ),
-                  helperText: 'Helper text',
-                  suffixIcon: Icon(
-                    Icons.check_circle,
-                  ),
-                  enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Color(0xFF6200EE)),
-                  ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 40.0),
+            child: TextFormField(
+              onChanged: (value) {
+                profName = value;
+              },
+              cursorColor: Theme.of(context).cursorColor,
+              initialValue: 'Insert Name',
+              maxLength: 20,
+              decoration: InputDecoration(
+                icon: Icon(Icons.favorite),
+                labelText: 'Professor Name',
+                labelStyle: TextStyle(
+                  color: Color(0xFF6200EE),
+                ),
+                helperText: 'Helper text',
+                suffixIcon: Icon(
+                  Icons.check_circle,
+                ),
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Color(0xFF6200EE)),
                 ),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.only(right: 40.0),
-              child: TextFormField(
-                onChanged: (value) {
-                  course = value;
-                },
-                cursorColor: Theme.of(context).cursorColor,
-                initialValue: 'Insert Course',
-                maxLength: 20,
-                decoration: InputDecoration(
-                  icon: Icon(Icons.favorite),
-                  labelText: 'Course Related to Book',
-                  labelStyle: TextStyle(
-                    color: Color(0xFF6200EE),
-                  ),
-                  helperText: 'Helper text',
-                  suffixIcon: Icon(
-                    Icons.check_circle,
-                  ),
-                  enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Color(0xFF6200EE)),
-                  ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 40.0),
+            child: TextFormField(
+              onChanged: (value) {
+                course = value;
+              },
+              cursorColor: Theme.of(context).cursorColor,
+              initialValue: 'Insert Course',
+              maxLength: 20,
+              decoration: InputDecoration(
+                icon: Icon(Icons.favorite),
+                labelText: 'Course Related to Book',
+                labelStyle: TextStyle(
+                  color: Color(0xFF6200EE),
+                ),
+                helperText: 'Helper text',
+                suffixIcon: Icon(
+                  Icons.check_circle,
+                ),
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Color(0xFF6200EE)),
                 ),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.only(right: 40.0),
-              child: TextFormField(
-                onChanged: (value) {
-                  sellerName = value;
-                },
-                cursorColor: Theme.of(context).cursorColor,
-                initialValue: 'Insert Name',
-                maxLength: 20,
-                decoration: InputDecoration(
-                  icon: Icon(Icons.favorite),
-                  labelText: 'Seller Name',
-                  labelStyle: TextStyle(
-                    color: Color(0xFF6200EE),
-                  ),
-                  helperText: 'Helper text',
-                  suffixIcon: Icon(
-                    Icons.check_circle,
-                  ),
-                  enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Color(0xFF6200EE)),
-                  ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 40.0),
+            child: TextFormField(
+              onChanged: (value) {
+                sellerName = value;
+              },
+              cursorColor: Theme.of(context).cursorColor,
+              initialValue: 'Insert Name',
+              maxLength: 20,
+              decoration: InputDecoration(
+                icon: Icon(Icons.favorite),
+                labelText: 'Seller Name',
+                labelStyle: TextStyle(
+                  color: Color(0xFF6200EE),
+                ),
+                helperText: 'Helper text',
+                suffixIcon: Icon(
+                  Icons.check_circle,
+                ),
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Color(0xFF6200EE)),
                 ),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.only(right: 40.0),
-              child: TextFormField(
-                onChanged: (value) {
-                  price = int.parse(value);
-                },
-                cursorColor: Theme.of(context).cursorColor,
-                initialValue: 'Insert Price',
-                maxLength: 20,
-                decoration: InputDecoration(
-                  icon: Icon(Icons.favorite),
-                  labelText: 'Book Price',
-                  labelStyle: TextStyle(
-                    color: Color(0xFF6200EE),
-                  ),
-                  helperText: 'Helper text',
-                  suffixIcon: Icon(
-                    Icons.check_circle,
-                  ),
-                  enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Color(0xFF6200EE)),
-                  ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 40.0),
+            child: TextFormField(
+              onChanged: (value) {
+                price = double.parse(value);
+              },
+              cursorColor: Theme.of(context).cursorColor,
+              initialValue: 'Insert Price',
+              maxLength: 20,
+              decoration: InputDecoration(
+                icon: Icon(Icons.favorite),
+                labelText: 'Book Price',
+                labelStyle: TextStyle(
+                  color: Color(0xFF6200EE),
+                ),
+                helperText: 'Helper text',
+                suffixIcon: Icon(
+                  Icons.check_circle,
+                ),
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Color(0xFF6200EE)),
                 ),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.only(right: 40.0),
-              child: TextFormField(
-                onChanged: (value) {
-                  condition = value;
-                },
-                cursorColor: Theme.of(context).cursorColor,
-                initialValue: 'Insert Condition',
-                maxLength: 20,
-                decoration: InputDecoration(
-                  icon: Icon(Icons.favorite),
-                  labelText: 'Book Condition',
-                  labelStyle: TextStyle(
-                    color: Color(0xFF6200EE),
-                  ),
-                  helperText: 'Helper text',
-                  suffixIcon: Icon(
-                    Icons.check_circle,
-                  ),
-                  enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Color(0xFF6200EE)),
-                  ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 40.0),
+            child: TextFormField(
+              onChanged: (value) {
+                condition = value;
+              },
+              cursorColor: Theme.of(context).cursorColor,
+              initialValue: 'Insert Condition',
+              maxLength: 20,
+              decoration: InputDecoration(
+                icon: Icon(Icons.favorite),
+                labelText: 'Book Condition',
+                labelStyle: TextStyle(
+                  color: Color(0xFF6200EE),
+                ),
+                helperText: 'Helper text',
+                suffixIcon: Icon(
+                  Icons.check_circle,
+                ),
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Color(0xFF6200EE)),
                 ),
               ),
             ),
-            Divider(),
-            Container(
-                child: picked == false
-                    ? Padding(
-                        padding: const EdgeInsets.only(left: 40.0),
-                        child: Container(
-                          alignment: Alignment.centerLeft,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              RaisedButton(
-                                color: Colors.blueAccent,
-                                onPressed: () {
-                                  showDialog(
-                                      context: context,
-                                      builder: (ctxt) {
-                                        return DialogWid(
-                                          camera: () {
-                                            _getFromCamera();
-                                          },
-                                          gallery: () {
-                                            _getFromGallery();
-                                          },
-                                        );
-                                      });
-                                },
-                                child: Text(
-                                  " Add Book Picture",
-                                  style: TextStyle(fontSize: 15),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      )
-                    : Container(
-                        height: 100,
-                        child: Image.file(
-                          imageFile,
-                          fit: BoxFit.cover,
-                        ),
-                      ))
-          ],
-        ),
+          ),
+          Divider(),
+          Container(
+              child: Container(
+            alignment: Alignment.centerLeft,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                RaisedButton(
+                  color: Colors.blueAccent,
+                  onPressed: () {
+                    showDialog(
+                        context: context,
+                        builder: (ctxt) {
+                          return DialogWid(
+                            camera: () {
+                              _getFromCamera();
+                            },
+                            gallery: () {
+                              _getFromGallery();
+                            },
+                          );
+                        });
+                  },
+                  child: Center(
+                    child: Text(
+                      " Add Book Picture",
+                      style: TextStyle(fontSize: 15),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )),
+          picked
+              ? Align(
+                  alignment: Alignment.centerLeft,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 15.5),
+                    child: Container(
+                      height: 200,
+                      width: 200,
+                      child: Image.file(
+                        imageFile,
+                        fit: BoxFit.fitHeight,
+                      ),
+                    ),
+                  ),
+                )
+              : SizedBox.shrink(),
+          Container(
+            alignment: Alignment.centerLeft,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                RaisedButton(
+                  color: Colors.blueAccent,
+                  onPressed: () {
+                    if (checkFields() == true) {
+                      print('worked');
+
+                      var bookData = {
+                        'bookname': bookName, //put your bookname variable here
+                        'sellerId': sellerName, //this ill get after auth
+                        'course': course, //put your course variable here
+                        'price': price, //put your price variable here
+                        'picFile': imageFile, //put your file variable here
+                        'condition':
+                            condition, //put your condition variable here
+                      };
+                    } else {
+                      print('fields are empty');
+                    }
+                  },
+                  child: Center(
+                    child: Text(
+                      " Upload Book",
+                      style: TextStyle(fontSize: 15),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
@@ -284,11 +325,28 @@ class DialogWid extends StatelessWidget {
   }
 }
 
-var bookData = {
-  'bookname': 'Book book book', //put your bookname variable here
-  'sellerId': 'siranov', //this ill get after auth
-  'course': 'Phys 53', //put your course variable here
-  'price': 26, //put your price variable here
-  'picFile': '', //put your file variable here
-  'condition': 'Good', //put your condition variable here
-};
+checkFields() {
+  bool everythingIsCorrect = true;
+  if (bookName == '') {
+    everythingIsCorrect = false;
+  }
+  if (profName == '') {
+    everythingIsCorrect = false;
+  }
+  if (course == '') {
+    everythingIsCorrect = false;
+  }
+  if (sellerName == '') {
+    everythingIsCorrect = false;
+  }
+  if (price < 0) {
+    everythingIsCorrect = false;
+  }
+  if (condition == '') {
+    everythingIsCorrect = false;
+  }
+  if (imageFile == null) {
+    everythingIsCorrect = false;
+  }
+  return everythingIsCorrect;
+}
